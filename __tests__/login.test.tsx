b1b92e2fdfda2459fc5e88b454384372e0b70c73
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-01-20 03:04:14 -0300

    axios install and change tests folder

--- __tests__/login.test.tsx
+++ __tests__/login.test.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
-import Login from "../index";
+import Login from "../app/(tabs)/index";
 
 describe("<Login />", () => {
   it("has 1 child", () => {
     const tree = renderer.create(<Login />).toJSON();
-    expect(tree.children.length).toBe(2);
+    expect(tree.children.length).toBe(3);
   });
 });
