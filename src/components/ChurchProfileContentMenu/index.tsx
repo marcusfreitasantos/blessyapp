80becead50c13d3bdafea147e0c7ec5b90bbbdfe
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-02-14 19:34:04 -0300

    [TEST] Unit test for content menu component

--- src/components/ChurchProfileContentMenu/index.tsx
+++ src/components/ChurchProfileContentMenu/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useContext } from "react";
 import { ChurchContentGlobalContext } from "@/contexts/currentChurchContent";
 
 import { HStack, ScrollView, Box } from "@gluestack-ui/themed";
