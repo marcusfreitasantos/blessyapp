4fb2421800487455bdedbee1e9952a681b084762
Marcus Freitas <marcusfreitasantos@gmail.com>
2024-02-16 08:25:15 -0300

    [TEST] Unit test for heading component

---
+++ src/components/Heading/Heading.test.tsx
@@ -0,0 +1,15 @@
+import { render } from "@testing-library/react-native";
+import HeadingComponent from ".";
+import { StyledProvider } from "@gluestack-style/react";
+import { config } from "../../../config/gluestack-ui.config";
+
+describe("Heading Component", () => {
+  it("Should render the heading component with props", () => {
+    const headingComponent = render(
+      <StyledProvider config={config}>
+        <HeadingComponent headingText="Heading Text" />
+      </StyledProvider>
+    );
+    expect(headingComponent).toBeTruthy();
+  });
+});
